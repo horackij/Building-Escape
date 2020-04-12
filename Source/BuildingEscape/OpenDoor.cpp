ea71fdb6b376a8a76abf08134b7a262a36eb3aa9
Joe Horacki <prince_937@hotmail.com>
2020-04-12 15:01:10 -0600

    Lecture 100 - Using Linear Interpolation

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -20,12 +20,7 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FRotator CurrentRotation = GetOwner()->GetActorRotation();
-
-	CurrentRotation.Yaw = 90.f;
-	//FRotator OpenDoor = {0.f, 90.f,0.f};
-
-	GetOwner()->SetActorRotation(CurrentRotation);
+	
 
 	
 }
@@ -36,6 +31,13 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is %f"), GetOwner()->GetActorRotation().Yaw);
+	
+	float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	FRotator OpenDoor(0.f,TargetYaw,0.f);
+	OpenDoor.Yaw = FMath::Lerp(CurrentYaw, TargetYaw, 0.02f);
+	GetOwner()->SetActorRotation(OpenDoor);
+
 }
 
